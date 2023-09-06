dfb4fd42ae3507c1ccb2114138ee3577c2563dc6
AndreaRomano02 <andy.romano2002@gmail.com>
2023-09-06 14:34:17 +0200

    Add in ProjectSeeder random type_id

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class ProjectSeeder extends Seeder
 {
@@ -14,11 +16,14 @@ class ProjectSeeder extends Seeder
    */
   public function run(Generator $faker): void
   {
+    $types_ids = Type::pluck('id')->toArray();
+
     for ($i = 0; $i < 50; $i++) {
       $project = new Project();
       $project->title = $faker->jobTitle();
       $project->url = $faker->url;
-      $project->description = $faker->paragraphs(5, true);;
+      $project->description = $faker->paragraphs(5, true);
+      $project->type_id = Arr::random($types_ids);
       $project->save();
     }
   }
